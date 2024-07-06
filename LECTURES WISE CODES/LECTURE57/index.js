87ee51db565d7734352665ad4d3379df1e84967b
Prathamesh <prathameshaj2004@gmail.com>
2024-07-06 20:57:24 +0530

    changes

---
+++ LECTURES WISE CODES/LECTURE57/index.js
@@ -0,0 +1,37 @@
+
+console.log("I am a tutorial on Loops")
+
+let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) { 
+//     console.log(a + i); 
+// }
+
+// let obj = {
+//     name: "Prathamesh",
+//     role: "Programmer",
+//     company: "AI TECH Devolper"
+// }
+ 
+// for (const key in obj) {  
+//         console.log(key)
+// }
+
+// for (const c of "prathamesh") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i<60000) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
