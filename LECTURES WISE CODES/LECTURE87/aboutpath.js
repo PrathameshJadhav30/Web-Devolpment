1f987aaf6cc759f0ce9c1e8d61a7e1320b40c49c
Prathamesh <prathameshaj2004@gmail.com>
2024-07-12 09:16:14 +0530

    Changes

---
+++ LECTURES WISE CODES/LECTURE87/aboutpath.js
@@ -0,0 +1,9 @@
+import path from "path"
+
+let myPath = "D:\\SIGMA_WEB_DEVOLPMENT\\harry.txt"
+console.log(path.extname(myPath))
+
+console.log(path.dirname(myPath))
+console.log(path.basename(myPath))
+
+console.log(path.join("c:/", "programs\\harry.txt"))
